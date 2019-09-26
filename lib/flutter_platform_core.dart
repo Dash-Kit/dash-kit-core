1bd5ba396da27e2714aa669bfd7d6c77687cf6ee
Maxim <max.mrtnv@gmail.com>
2019-09-27 00:36:36 +0400

    Configuration: Export of Core components

--- lib/flutter_platform_core.dart
+++ lib/flutter_platform_core.dart
@@ -1,1 +1,15 @@
 library flutter_platform_core;
+
+export './action.dart';
+export './reducer.dart';
+export './state.dart';
+export './store_provider.dart';
+
+export './root_epic.dart';
+export './root_reducer.dart';
+
+export './store_list.dart';
+
+export './tests/epic_test_core.dart';
+
+export './action_dispatcher_middleware.dart';
