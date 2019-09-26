bc84005baf4cb5f861bedd9d3fe7bd106e92686f
Denis Arkhireev <den-uln@mail.ru>
2019-09-26 19:08:28 +0400

    Updated dependency versions, added equatable, changed action

--- lib/Action.dart
+++ lib/Action.dart
@@ -1,4 +1,8 @@
-abstract class Action {
+import 'package:equatable/equatable.dart';
+
+abstract class Action extends Equatable {
+  const Action([List equatableProps = const []]) : super(equatableProps);
+
   @override
   String toString() {
     var actionName = runtimeType.toString().replaceAll(RegExp(r'Action'), '');
