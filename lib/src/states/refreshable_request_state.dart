3dc76c06fcf79ce8a6a35e141033944a5d196e9c
Artem Abramov <artem.abramov@dualbootpartners.com>
2020-08-10 20:49:42 +0300

    Refactored request states

--- lib/src/states/refreshable_request_state.dart
+++ lib/src/states/refreshable_request_state.dart
@@ -1,35 +1,12 @@
-class RefreshableRequestState {
-  const RefreshableRequestState._(this.name);
+enum RefreshableRequestState {
+  idle,
+  inProgress,
+  refreshing,
+  success,
+  error,
+}
 
-  final String name;
-
-  @override
-  bool operator ==(Object other) =>
-      identical(this, other) ||
-      other is RefreshableRequestState &&
-          runtimeType == other.runtimeType &&
-          name == other.name;
-
-  @override
-  int get hashCode => name.hashCode;
-
-  static const RefreshableRequestState idle = RefreshableRequestState._('idle');
-  static const RefreshableRequestState inProgress =
-      RefreshableRequestState._('inProgress');
-  static const RefreshableRequestState refreshing =
-      RefreshableRequestState._('refreshing');
-  static const RefreshableRequestState success =
-      RefreshableRequestState._('success');
-  static const RefreshableRequestState error =
-      RefreshableRequestState._('error');
-  static const values = [
-    idle,
-    inProgress,
-    refreshing,
-    success,
-    error,
-  ];
-
+extension RefrershableRequestStateExtension on RefreshableRequestState {
   bool get isIdle => this == RefreshableRequestState.idle;
 
   bool get isInProgress => this == RefreshableRequestState.inProgress;
