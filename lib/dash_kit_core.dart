af00a06e60d8b4de4f7f146b1f10cf5faa1f99da
Maxim Martynov <max.mrtnv@gmail.com>
2020-09-29 02:16:25 +0400

    Configuration: Exported Async Redux & Built Collection packages with Dash Kit Core

--- lib/dash_kit_core.dart
+++ lib/dash_kit_core.dart
@@ -9,3 +9,5 @@ export './src/utils/store_list.dart';
 export './src/states/paginated_list.dart';
 export './src/states/operation_state.dart';
 
+export 'package:built_collection/built_collection.dart';
+export 'package:async_redux/async_redux.dart' hide Store;
