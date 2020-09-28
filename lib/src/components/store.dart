b36effa185ba9e5ae36801ef9beeeb8134b72d38
Maxim Martynov <max.mrtnv@gmail.com>
2020-09-29 02:18:28 +0400

    Refactoring: Store implementation

---
+++ lib/src/components/store.dart
@@ -0,0 +1,22 @@
+import 'package:async_redux/async_redux.dart' as async_redux;
+import 'package:dash_kit_core/dash_kit_core.dart';
+
+class Store<S extends GlobalState> extends async_redux.Store<S> {
+  Store({
+    S initialState,
+    List<ActionObserver> actionObservers,
+    List<StateObserver> stateObservers,
+    Persistor persistor,
+    ModelObserver modelObserver,
+    ErrorObserver errorObserver,
+    WrapError wrapError,
+  }) : super(
+          initialState: initialState,
+          actionObservers: actionObservers,
+          stateObservers: stateObservers,
+          persistor: persistor,
+          modelObserver: modelObserver,
+          errorObserver: errorObserver,
+          wrapError: wrapError,
+        );
+}
