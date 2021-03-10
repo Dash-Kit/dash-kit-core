01a97c4909f59f9b1f33a65822ed9c107e233b4e
Artem Abramov <artem.abramov@dualbootpartners.com>
2021-03-10 15:47:56 +0400

    Fixed an issue with types

--- lib/src/components/store.dart
+++ lib/src/components/store.dart
@@ -1,7 +1,7 @@
 import 'package:async_redux/async_redux.dart' as async_redux;
 import 'package:dash_kit_core/dash_kit_core.dart';
 
-class Store<S extends GlobalState> extends async_redux.Store<S?> {
+class Store<S extends GlobalState> extends async_redux.Store<S> {
   Store({
     required S initialState,
     List<ActionObserver>? actionObservers,
