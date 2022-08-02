ece1b576bb282e8ad5f79c91e976b553be3fd3a2
artem.abramov <artem.abramov@dualbootpartners.com>
2022-08-02 15:50:27 +0400

    Refactored mutation methods in StoreList to return the new instance of store list instead

--- lib/src/utils/store_list.dart
+++ lib/src/utils/store_list.dart
@@ -1,4 +1,5 @@
 import 'package:built_collection/built_collection.dart';
+import 'package:dash_kit_core/dash_kit_core.dart';
 
 abstract class StoreListItem {
   Object get id;
@@ -12,7 +13,7 @@ class StoreList<T extends StoreListItem> {
 
   StoreList._(this._itemsIds, this._items, this._itemListCache);
 
-  factory StoreList([List<T> list = const []]) {
+  factory StoreList([Iterable<T> list = const []]) {
     final filteredItems = list.toBuiltList();
 
     final itemListCache = filteredItems;
@@ -38,59 +39,46 @@ class StoreList<T extends StoreListItem> {
 
   T? getItem(Object id) => _items[id];
 
-  void updateList(Iterable<T> items) {
-    final filteredItems = items.toBuiltList();
-
-    _itemListCache = filteredItems;
-    _itemsIds = filteredItems.map((i) => i.id).toBuiltList();
-
-    filteredItems.forEach((i) => _items = _items.rebuild((b) => b[i.id] = i));
-  }
-
-  void addItem(Object? id, T? value) {
+  StoreList<T> addItem(Object? id, T? value) {
     if (id == null || value == null) {
-      return;
+      return this;
     }
 
-    _itemListCache = _itemListCache?.rebuild((b) => b.add(value));
+    final updatedItems = items.rebuild((b) => b.add(value));
 
-    _items = _items.rebuild((b) => b[id] = value);
-    _itemsIds = _itemsIds.rebuild((b) => b.add(id));
+    return StoreList(updatedItems);
   }
 
-  void addAll(Iterable<T> values) {
+  StoreList<T> addAll(Iterable<T> values) {
     if (values.isEmpty) {
-      return;
+      return this;
     }
 
-    _itemListCache = _itemListCache?.rebuild((b) => b.addAll(values));
+    final updatedItems = items.rebuild((b) => b.addAll(values));
 
-    _items = _items.rebuild(
-      (b) => values.forEach((value) => b[value.id] = value),
-    );
-
-    _itemsIds = _itemsIds.rebuild(
-      (b) => b.addAll(values.map((item) => item.id).toList()),
-    );
+    return StoreList(updatedItems);
   }
 
-  void updateItem(Object id, T value) {
-    _itemListCache = null;
-    _items = _items.rebuild((b) => b[id] = value);
+  StoreList<T> updateItem(Object? id, T? value) {
+    if (id == null || value == null) {
+      return this;
+    }
+
+    final updateIndex = items.indexWhere((e) => e.id == id);
+
+    if (updateIndex < 0) {
+      return this;
+    }
+
+    final updatedItems = items.rebuild((b) => b[updateIndex] = value);
+
+    return StoreList(updatedItems);
   }
 
-  void deleteItem(Object id) {
-    _itemListCache = null;
+  StoreList<T> deleteItem(Object id) {
+    final updatedItems = items.rebuild((b) => b.removeWhere((e) => e.id == id));
 
-    _items = _items.rebuild((b) => b.remove(id));
-    _itemsIds = _itemsIds.rebuild((b) => b.removeWhere((i) => i == id));
-  }
-
-  void clear() {
-    _itemListCache = null;
-
-    _items = _items.rebuild((b) => b.clear());
-    _itemsIds = _itemsIds.rebuild((b) => b.clear());
+    return StoreList(updatedItems);
   }
 
   @override
--- test/store_list_test.dart
+++ test/store_list_test.dart
@@ -38,49 +38,16 @@ void main() {
     expect(storeList.itemsMap.keys.toList(), equals([1]));
   });
 
-  test('List of items is updated', () {
-    final storeList = StoreList([
-      NumberStoreListItem(1),
-      NumberStoreListItem(2),
-      NumberStoreListItem(3),
-    ]);
-
-    expect(storeList.itemsIds, equals([1, 2, 3]));
-    expect(storeList.items.map((i) => i.id), equals([1, 2, 3]));
-
-    storeList.updateList([
-      NumberStoreListItem(2),
-      NumberStoreListItem(4),
-      NumberStoreListItem(5),
-      NumberStoreListItem(6),
-    ]);
-
-    expect(storeList.itemsIds, equals([2, 4, 5, 6]));
-    expect(storeList.items.map((i) => i.id), equals([2, 4, 5, 6]));
-  });
-
-  test('Item is updated in list', () {
-    final storeList = StoreList(<StoreListItem>[NumberStoreListItem(1)]);
-
-    expect(storeList.itemsIds, equals([1]));
-    expect(storeList.items.map((i) => i.id), equals([1]));
-
-    storeList.updateItem(1, NumberStoreListItem(3));
-
-    expect(storeList.itemsIds, equals([1]));
-    expect(storeList.items.map((i) => i.id), equals([3]));
-  });
-
   test('Incorrect id is ignored on updating item in list', () {
     final storeList = StoreList(<StoreListItem>[NumberStoreListItem(1)]);
 
     expect(storeList.itemsIds, equals([1]));
     expect(storeList.items.map((i) => i.id), equals([1]));
 
-    storeList.updateItem(2, NumberStoreListItem(3));
+    final updatedStoreList = storeList.updateItem(2, NumberStoreListItem(3));
 
-    expect(storeList.itemsIds, equals([1]));
-    expect(storeList.items.map((i) => i.id), equals([1]));
+    expect(updatedStoreList.itemsIds, equals([1]));
+    expect(updatedStoreList.items.map((i) => i.id), equals([1]));
   });
 
   test('Item is deleted from list', () {
@@ -90,10 +57,10 @@ void main() {
       NumberStoreListItem(3),
     ]);
 
-    storeList.deleteItem(2);
+    final updatedStoreList = storeList.deleteItem(2);
 
-    expect(storeList.itemsIds, equals([1, 3]));
-    expect(storeList.items.map((i) => i.id), equals([1, 3]));
+    expect(updatedStoreList.itemsIds, equals([1, 3]));
+    expect(updatedStoreList.items.map((i) => i.id), equals([1, 3]));
   });
 
   test('Incorrect id is ignored on deleting item from list', () {
@@ -112,19 +79,6 @@ void main() {
     expect(storeList.items.map((i) => i.id), equals([1, 2, 3]));
   });
 
-  test('Items is deleted from list on clear', () {
-    final storeList = StoreList([
-      NumberStoreListItem(1),
-      NumberStoreListItem(2),
-      NumberStoreListItem(3),
-    ]);
-
-    storeList.clear();
-
-    expect(storeList.itemsIds, equals([]));
-    expect(storeList.items.map((i) => i.id), equals([]));
-  });
-
   test('All of items were added', () {
     final storeList = StoreList([
       NumberStoreListItem(1),
@@ -135,13 +89,13 @@ void main() {
     expect(storeList.itemsIds, equals([1, 2, 3]));
     expect(storeList.items.map((i) => i.id), equals([1, 2, 3]));
 
-    storeList.addAll([
+    final updatedStoreList = storeList.addAll([
       NumberStoreListItem(4),
       NumberStoreListItem(5),
       NumberStoreListItem(6),
     ]);
 
-    expect(storeList.itemsIds, equals([1, 2, 3, 4, 5, 6]));
-    expect(storeList.items.map((i) => i.id), equals([1, 2, 3, 4, 5, 6]));
+    expect(updatedStoreList.itemsIds, equals([1, 2, 3, 4, 5, 6]));
+    expect(updatedStoreList.items.map((i) => i.id), equals([1, 2, 3, 4, 5, 6]));
   });
 }
